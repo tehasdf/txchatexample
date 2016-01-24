4e02f6f3c46860e922a7698c45c7c571de5ab890
asdf <tehasdf@gmail.com>
2016-01-24 17:43:13 +0100

    factor out the query rendering function

--- txchatexample/util.py
+++ txchatexample/util.py
@@ -19,27 +19,29 @@ def dumps(obj, encoder=JSONEncoderWithDatetime()):
     return encoder.encode(obj)
 
 
+def getQueryWithParams(dialect, query, params):
+    if hasattr(query, 'compile'):
+        compiled = query.compile(dialect=dialect)
+        query_params = dict(compiled.params)
+        if params is not None:
+            query_params.update(params)
+        return str(compiled), query_params
+    return query, params
+
+
 class ConnectionPool(txpostgres.ConnectionPool):
     def __init__(self, **kwargs):
         kwargs.setdefault('cursor_factory', NamedTupleCursor)
         self._sqla_dialect = dialect()
         super(ConnectionPool, self).__init__(None, **kwargs)
 
-    def _getQueryWithParams(self, query, params):
-        if hasattr(query, 'compile'):
-            compiled = query.compile(dialect=self._sqla_dialect)
-            query_params = dict(compiled.params)
-            if params is not None:
-                query_params.update(params)
-            return str(compiled), query_params
-        return query, params
 
     def runQuery(self, query, params=None):
-        query, params = self._getQueryWithParams(query, params)
+        query, params = getQueryWithParams(self._sqla_dialect, query, params)
         return super(ConnectionPool, self).runQuery(query, params)
 
     def runOperation(self, query, params=None):
-        query, params = self._getQueryWithParams(query, params)
+        query, params = getQueryWithParams(self._sqla_dialect, query, params)
         return super(ConnectionPool, self).runOperation(query, params)
 
 
